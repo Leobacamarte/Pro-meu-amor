ac29fa8893c45054ca7fcb163a4852f965acb226
Leonardo Gonçalves Martins de Lima <martinsleo1999@gmail.com>
2023-06-17 02:04:59 -0300

    proMeuAmor

---
+++ script.js
@@ -0,0 +1,17 @@
+const milMilhoesBtn = document.querySelector('#milM');
+
+milMilhoesBtn.addEventListener('click', () => {
+    window.alert('Eu também te amo mil milhões, meu amor!')
+    location.href = "https://www.youtube.com/watch?v=har3c93pJe0";
+});
+
+const desvia = (btn) => {
+  btn.style.position = 'absolute';
+  btn.style.bottom = geraPosicao(10, 90);
+  btn.style.left = geraPosicao(10, 90);
+  console.log('opa, desviei...');
+}
+
+const geraPosicao = (min, max) => {
+  return (Math.random() * (max - min) + min) + "%";
+}
